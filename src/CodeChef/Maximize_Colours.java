02f07159f2eacf399e302d58eceffa20c9998b83
Rishi <rishabhkumar.rk755@gmail.com>
2025-02-24 19:17:26 +0530

    C:\DSA\src\CodeChef\Maximize_Colours.java

---
+++ src/CodeChef/Maximize_Colours.java
@@ -0,0 +1,15 @@
+package CodeChef;
+
+public class Maximize_Colours
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static int Colours(int x, int y, int z)
+    {
+        if(x==0 && y==0 && z==0) return 0;
+        
+    }
+}
